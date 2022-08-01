60eba54326ce4f3134350bf3a2e4fc5c494f5ee8
socratech <criosro@unal.edu.co>
2022-07-31 19:29:07 -0500

    Github repository setup

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct parsed_line parsed_line_type;
+
+enum func_status {
+    SUCCESS = 0,
+    FAILURE = -1,
+    EXITED = 1
+};
+
+struct parsed_line {
+    char command[20];
+    char options[20];
+    char argument[60];
+};
+
+int terminal_read_line() {
+    static char line_buffer[100];
+
+    // Reads and validates the commands from the command line.
+
+    return SUCCESS;
+}
+
+parsed_line_type terminal_line_parse(char parsed_line[]) {
+    // Parses the line and returns it as a parsed_line_type.
+}
+
+int terminal_execute_line() {
+
+    // Executes the commands
+
+    return SUCCESS;
+}
+
 int main(void) {
+
     return EXIT_SUCCESS;
 }
 
